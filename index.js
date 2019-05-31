3ddf37718120220ed6c212dbd00eb475b260ee95
Philip Roth <phil.roth077@gmail.com>
2019-05-31 00:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var counter = []
-  for (var i = 0; i < 3; i++) {
-    counter.push(musicians[i] " plays " instruments[i])
-  }
-
-
-}
+// // add solution here
+// function theBeatlesPlay(musicians, instruments) {
+//   var counter = []
+//   for (var i = 0; i < 3; i++) {
+//     counter.push(musicians[i] " plays " instruments[i])
+//   }
+//
+//
+// }
