2d7cd19969227b40b25d3fa6c562974a1a92681c
Philip Roth <phil.roth077@gmail.com>
2019-05-31 00:25:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var counter = []
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 3; i++) {
     counter.push(musicians[i] " plays " instruments[i])
   }
   return counter
